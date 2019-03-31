f55b6a49be7af6b31aec08c9ab9e3c46c647ee3e
Temirlan <bamblebee988@gmail.com>
2019-03-31 21:39:11 +0600

    add item list

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,22 +7,33 @@ import PersonDetails from '../person-details';
 
 import './app.css';
 
-const App = () => {
-  return (
+export default class App extends React.Component {
+
+  state = {
+    selectedPerson: null
+  };
+  
+  onPersonSelected = (id) => {
+    this.setState({
+      selectedPerson: id
+    });
+  };
+
+  render() {
+    return (
     <div className="container">
       <Header />
       <RandomPlanet />
 
       <div className="row mb2">
         <div className="col-md-6">
-          <ItemList />
+          <ItemList onItemSelected={this.onPersonSelected} />
         </div>
         <div className="col-md-6">
-          <PersonDetails />
+          <PersonDetails personId={this.state.selectedPerson} />
         </div>
       </div>
     </div>
-  );
+    )
+  };
 };
-
-export default App;
